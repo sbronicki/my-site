577bc0a7bd7d3cec4a989d44bbea69dc90197e1d
sbronicki <sbronicki@gmail.com>
2020-10-09 03:19:49 -0400

    small fixes + media section responsive hack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navbar = document.getElementById('navbar');
 let scrolled = false;
 
 window.onscroll = function() {
-	if (window.pageYOffset > 200) {
+	if (window.pageYOffset > 600) {
 		navbar.classList.remove('top');
 		setTimeout(function() {
 			navbar.style.transform = 'translateY(0)';
@@ -13,3 +13,25 @@ window.onscroll = function() {
 		scrolled = false;
 	}
 };
+
+// //////////////////
+
+if (window.innerWidth < 768) {
+	document.getElementById('mediaSection').innerHTML = `
+<div class="row">
+    <div class="column">
+        <div class="column-1">
+            <img src="Scan 9 copy.jpeg" alt="">
+        </div>
+    </div>
+    <div class="column">
+        <div class="column-2">
+            <div>
+                <h3 class="skills">Film & Photography/Media Editing</h3>
+                <p>Photoshop, Final Cut Pro</p>
+                <h2><a href="https://www.youtube.com/nimso9">My YouTube</a></h2>
+            </div>
+        </div>
+    </div>
+</div> `;
+}
